67b3ddd13a47bbd8e2a0af433427ef89c87cd6e9
erdal.gunes <erdalgns@gmail.com>
2016-07-17 18:57:24 +0300

    Fixed mock selection crash issue

--- client/app/src/main/java/com/xbenjii/pokemock/MainActivity.java
+++ client/app/src/main/java/com/xbenjii/pokemock/MainActivity.java
@@ -1,15 +1,20 @@
 package com.xbenjii.pokemock;
 
 import android.Manifest;
+import android.app.AppOpsManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.StrictMode;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,11 +34,15 @@ public class MainActivity extends AppCompatActivity implements MyBroadcastReceiv
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 12;
     private LocationListener locationListener;
 
+    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -70,29 +79,45 @@ public class MainActivity extends AppCompatActivity implements MyBroadcastReceiv
 
         textView.setFocusable(false);
 
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String ipAddress = editText.getText().toString();
-                if(!isIP(ipAddress)) {
-                    textView.append("Not a valid IP address: " + ipAddress + "\n");
-                    return;
+                boolean isMockLocationEnabled = isMockSettingsON();
+                if (!isMockLocationEnabled) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage("Mock location is disabled")
+                            .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Intent i = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+                                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(i);
+                                }
+                            }).setCancelable(false);
+                    builder.create();
+                    builder.show();
+                } else {
+                    final String ipAddress = editText.getText().toString();
+                    if (!isIP(ipAddress)) {
+                        textView.append("Not a valid IP address: " + ipAddress + "\n");
+                        return;
+                    }
+                    connectToWebSocket("ws://" + ipAddress);
                 }
-                connectToWebSocket("ws://" + ipAddress);
             }
         });
 
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_COARSE_LOCATION
             }, MY_PERMISSION_ACCESS_COARSE_LOCATION);
         } else {
             mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         }
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION
             }, MY_PERMISSION_ACCESS_FINE_LOCATION);
         } else {
@@ -130,6 +155,7 @@ public class MainActivity extends AppCompatActivity implements MyBroadcastReceiv
     private static final Pattern PATTERN = Pattern.compile(
             "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5]):(\\d+?)$"
     );
+
     private static boolean isIP(String ipAddress) {
         return PATTERN.matcher(ipAddress).matches();
     }
@@ -139,5 +165,22 @@ public class MainActivity extends AppCompatActivity implements MyBroadcastReceiv
         Intent intent = new Intent("address");
         intent.putExtra("address", address);
         sendBroadcast(intent);
+    }
+
+    public boolean isMockSettingsON() {
+        boolean isMockLocation = false;
+        try {
+            //if marshmallow
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                AppOpsManager opsManager = (AppOpsManager) this.getSystemService(Context.APP_OPS_SERVICE);
+                isMockLocation = (opsManager.checkOp(AppOpsManager.OPSTR_MOCK_LOCATION, android.os.Process.myUid(), BuildConfig.APPLICATION_ID) == AppOpsManager.MODE_ALLOWED);
+            } else {
+                // in marshmallow this will always return true
+                isMockLocation = !android.provider.Settings.Secure.getString(this.getContentResolver(), "mock_location").equals("0");
+            }
+        } catch (Exception e) {
+            return isMockLocation;
+        }
+        return isMockLocation;
     }
 }
